b1d2a90da21106ad212ca9754f16f0c1591e1843
Eirik Brandtzæg <eirikb@eirikb.no>
2012-02-19 13:01:52 +0100

    Removed unused snippets

--- examples/test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-sys = require('sys');
-
-var lett = require('../lett.js');
-
-fs.readFile(process.argv[2], function(e, d) {
-    console.log(sys.inspect(lett.letteval(d.toString()), true, null));
-});
-
